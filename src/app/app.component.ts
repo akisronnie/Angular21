96c82c97753f38c012d4ea5190a407298f556e69
Alexandr Kyslynskyi <alexandr.kyslynskyi@daxx.com>
2019-01-16 18:14:08 +0200

    rebuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  private deck: TCard[] = [];
-  private firstGame: boolean = true;
   public isShowResult: boolean = false;
   public message: string = 'Welcome to game';
   public computer: TPlayer = {
@@ -16,18 +14,88 @@ export class AppComponent implements OnInit {
     sum: 0,
     numberWins: 0,
   };
-
   public player: TPlayer = {
     hand: [],
     sum: 0,
     numberWins: 0,
   };
 
+  
+  private deck: TCard[] = [];
+  private firstGame: boolean = true;
+ 
+
   public ngOnInit(): void {
     this.deck = this.generateDeck();
     this.deck = this.deckSort(this.deck);
     this.startNewGame();
   }
+  public startNewGame(): void  {
+    if (!this.firstGame) {
+      this.finish();
+    }
+
+    this.message = 'Welcome to game';
+    this.firstGame = false;
+    this.player.hand.map((card: TCard) => { card.src = `../assets/img/outside.png`; });
+    this.computer.hand.map((card: TCard) => { card.src = `../assets/img/outside.png`; });
+    this.isShowResult = false;
+    this.deck = this.deck.concat(this.player.hand);
+    this.deck = this.deck.concat(this.computer.hand);
+    this.player.hand = [];
+    this.player.sum = 0;
+    this.computer.hand = [];
+    this.computer.sum = 0;
+    this.deck = this.deckSort(this.deck);
+    this.getYou();
+  }
+  public getYou(): void  {
+    this.player.hand.push(this.deck[this.deck.length - 1]);
+    this.deck.pop();
+    this.player.hand[this.player.hand.length - 1].src = `../assets/img/${this.player.hand[this.player.hand.length - 1].name}${this.player.hand[this.player.hand.length - 1].suits}.png`;
+    this.player.sum += this.player.hand[this.player.hand.length - 1].value;
+
+    if (this.player.sum > 21) {
+      this.message = 'You lose! That is enough already! Bust. You score :' + this.player.sum;
+      this.isShowResult = true;
+      this.computer.hand.map((card: TCard) => { card.src = `../assets/img/${card.name}${card.suits}.png`; });
+
+      return;
+    }
+    this.getComp();
+  }
+  public finish(): void  {
+    if (this.computer.sum <= 15) {
+      this.getComp();
+    }
+    this.computer.hand.map((card: TCard) => { card.src = `../assets/img/${card.name}${card.suits}.png`; });
+    this.isShowResult = true;
+
+    if (this.computer.sum > 21) {
+      this.message = 'At the computer bust!';
+      this.player.numberWins++;
+      this.firstGame = true;
+
+      return;
+    }
+
+    if (this.computer.sum === this.player.sum) {
+      this.message = 'DRAW!';
+
+      return;
+    }
+
+    if (this.computer.sum > this.player.sum || this.player.sum > 21) {
+      this.message = 'YOU LOSE!!!!! LOSER!!!';
+      this.computer.numberWins++;
+    } else {
+      this.message = 'YOU WIN!!!!! WINNER!!!';
+      this.player.numberWins++;
+    }
+
+    this.firstGame = true;
+  }
+
 
   private generateDeck (): TCard[] {
     const suits: string[] = ['♠', '♥', '♣', '♦'];
@@ -50,7 +118,6 @@ export class AppComponent implements OnInit {
     }
     return deck;
   }
-
   private deckSort(deck: TCard[]): TCard[] {
     let firstElem: number = 0;
     let secondElem: number = 0;
@@ -65,27 +132,7 @@ export class AppComponent implements OnInit {
     }
     return deck;
   }
-
-  public startNewGame(): void  {
-    if (!this.firstGame) {
-      this.finish();
-    }
-    this.message = 'Welcome to game';
-    this.firstGame = false;
-    this.player.hand.map((card: TCard) => { card.src = `../assets/img/outside.png`; });
-    this.computer.hand.map((card: TCard) => { card.src = `../assets/img/outside.png`; });
-    this.isShowResult = false;
-    this.deck = this.deck.concat(this.player.hand);
-    this.deck = this.deck.concat(this.computer.hand);
-    this.player.hand = [];
-    this.player.sum = 0;
-    this.computer.hand = [];
-    this.computer.sum = 0;
-    this.deck = this.deckSort(this.deck);
-    this.getYou();
-  }
-
-  public getComp(): void {
+  private getComp(): void {
     if (this.computer.sum <= 15) {
       this.computer.hand.push(this.deck[this.deck.length - 1]);
       this.deck.pop();
@@ -95,49 +142,5 @@ export class AppComponent implements OnInit {
     if (this.computer.sum > 21) {
       this.finish();
     }
-  }
-
-  public getYou(): void  {
-    this.player.hand.push(this.deck[this.deck.length - 1]);
-    this.deck.pop();
-    this.player.hand[this.player.hand.length - 1].src = `../assets/img/${this.player.hand[this.player.hand.length - 1].name}${this.player.hand[this.player.hand.length - 1].suits}.png`;
-    this.player.sum += this.player.hand[this.player.hand.length - 1].value;
-    if (this.player.sum > 21) {
-      this.message = 'You lose! That is enough already! Bust. You score :' + this.player.sum;
-      this.isShowResult = true;
-      this.computer.hand.map((card: TCard) => { card.src = `../assets/img/${card.name}${card.suits}.png`; });
-      return;
-    }
-    this.getComp();
-  }
-
-  public finish(): void  {
-    if (this.computer.sum <= 15) {
-      this.getComp();
-    }
-    this.computer.hand.map((card: TCard) => { card.src = `../assets/img/${card.name}${card.suits}.png`; });
-    this.isShowResult = true;
-
-    if (this.computer.sum > 21) {
-      this.message = 'At the computer bust!';
-      this.player.numberWins++;
-      this.firstGame = true;
-      return;
-    }
-
-    if (this.computer.sum === this.player.sum) {
-      this.message = 'DRAW!';
-      return;
-    }
-
-    if (this.computer.sum > this.player.sum || this.player.sum > 21) {
-      this.message = 'YOU LOSE!!!!! LOSER!!!';
-      this.computer.numberWins++;
-    } else {
-      this.message = 'YOU WIN!!!!! WINNER!!!';
-      this.player.numberWins++;
-    }
-
-    this.firstGame = true;
   }
 }
