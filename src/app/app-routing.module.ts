98847cbcc1b059c6f451c9959a0c6ebb354e2500
Alexandr Kyslynskyi <alexandr.kyslynskyi@daxx.com>
2019-01-23 17:20:41 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+  redirectTo: '/menu',
+  pathMatch: 'full'
+  }, {
+    path: 'game',
+    component: GameComponent
+  }, {
+    path: 'menu',
+    component: MenuComponent
+  }, {
+    path: '**',
+    component: ErrorComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ScoreComponent } from './score/score.component';
-import { FieldComponent } from './field/field.component';
+import { ScoreComponent } from './game/score/score.component';
+import { FieldComponent } from './game/field/field.component';
+import { GameComponent } from './game/game.component';
+import { MenuComponent } from './menu/menu.component';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
@@ -12,6 +15,9 @@ import { FieldComponent } from './field/field.component';
     AppComponent,
     ScoreComponent,
     FieldComponent,
+    GameComponent,
+    MenuComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
